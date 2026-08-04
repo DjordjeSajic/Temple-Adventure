36cf064f3d8385c7b39ec221aa93ad9cee006bac
DjordjeSajic <djordje.sajic5@gmail.com>
2026-08-04 10:50:33 +0200

    Create empty TempleAdventure

---
+++ app/include/TempleAdventure.hpp
@@ -0,0 +1,15 @@
+//
+// Created by djordjes on 8/4/26.
+//
+
+#ifndef MATF_RG_PROJECT_TEMPLEADVENTURE_HPP
+#define MATF_RG_PROJECT_TEMPLEADVENTURE_HPP
+#include <engine/core/Engine.hpp>
+
+namespace app {
+    class TempleAdventure : public engine::core::App {
+        void app_setup() override;
+    };
+} // app
+
+#endif //MATF_RG_PROJECT_TEMPLEADVENTURE_HPP
---
+++ app/src/TempleAdventure.cpp
@@ -0,0 +1,13 @@
+//
+// Created by djordjes on 8/4/26.
+//
+
+#include "../include/TempleAdventure.hpp"
+
+#include <spdlog/spdlog.h>
+
+namespace app {
+    void TempleAdventure::app_setup() {
+        spdlog::info("App setup works for Temple Adventure");
+    }
+} // app
