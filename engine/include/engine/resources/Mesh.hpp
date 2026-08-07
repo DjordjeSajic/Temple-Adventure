0b30c7f2146fa29cdaada83f1269485466fd3e52
DjordjeSajic <djordje.sajic5@gmail.com>
2026-08-07 16:38:48 +0200

    Added m_diffuse_color for model with no texture files only color values in mtl with logs for checking there value

--- engine/include/engine/resources/Mesh.hpp
+++ engine/include/engine/resources/Mesh.hpp
@@ -11,52 +11,53 @@
 #include <vector>
 
 namespace engine::resources {
-/**
-* @struct Vertex
-* @brief Represents a vertex in the mesh.
-*/
-struct Vertex {
-    glm::vec3 Position;
-    glm::vec3 Normal;
-    glm::vec2 TexCoords;
-
-    glm::vec3 Tangent;
-    glm::vec3 Bitangent;
-};
-
-/**
-* @class Mesh
-* @brief Represents a mesh in the model in the OpenGL context.
-*/
-class Mesh {
-    friend class AssimpSceneProcessor;
-
-public:
     /**
-    * @brief Draws the mesh using a given shader. Called by the @ref Model::draw function to draw all the meshes in the model.
-    * @param shader The shader to use for drawing.
+    * @struct Vertex
+    * @brief Represents a vertex in the mesh.
     */
-    void draw(const Shader *shader);
+    struct Vertex {
+        glm::vec3 Position;
+        glm::vec3 Normal;
+        glm::vec2 TexCoords;
+
+        glm::vec3 Tangent;
+        glm::vec3 Bitangent;
+    };
 
     /**
-    * @brief Destroys the mesh in the OpenGL context.
+    * @class Mesh
+    * @brief Represents a mesh in the model in the OpenGL context.
     */
-    void destroy();
+    class Mesh {
+        friend class AssimpSceneProcessor;
 
-private:
-    /**
-    * @brief Constructs a Mesh object.
-    * @param vertices The vertices in the mesh.
-    * @param indices The indices in the mesh.
-    * @param textures The textures in the mesh.
-     */
-    Mesh(const std::vector<Vertex> &vertices, const std::vector<uint32_t> &indices,
-         std::vector<Texture *> textures);
+    public:
+        /**
+        * @brief Draws the mesh using a given shader. Called by the @ref Model::draw function to draw all the meshes in the model.
+        * @param shader The shader to use for drawing.
+        */
+        void draw(const Shader *shader);
 
-    uint32_t m_vao{0};
-    uint32_t m_num_indices{0};
-    std::vector<Texture *> m_textures;
-};
-}// namespace engine::resources
+        /**
+        * @brief Destroys the mesh in the OpenGL context.
+        */
+        void destroy();
+
+    private:
+        /**
+        * @brief Constructs a Mesh object.
+        * @param vertices The vertices in the mesh.
+        * @param indices The indices in the mesh.
+        * @param textures The textures in the mesh.
+         */
+        Mesh(const std::vector<Vertex> &vertices, const std::vector<uint32_t> &indices,
+             std::vector<Texture *> textures, glm::vec3 diffuse_color = glm::vec3(1.0f));
+
+        uint32_t m_vao{0};
+        uint32_t m_num_indices{0};
+        std::vector<Texture *> m_textures;
+        glm::vec3 m_diffuse_color{1.0f, 1.0f, 1.0f};
+    };
+} // namespace engine::resources
 
 #endif//MATF_RG_PROJECT_MESH_HPP
--- engine/src/Mesh.cpp
+++ engine/src/Mesh.cpp
@@ -1,4 +1,3 @@
-
 // clang-format off
 #include <glad/glad.h>
 // clang-format on
@@ -9,66 +8,79 @@
 #include <unordered_map>
 
 namespace engine::resources {
+    Mesh::Mesh(const std::vector<Vertex> &vertices, const std::vector<uint32_t> &indices,
+               std::vector<Texture *> textures, glm::vec3 diffuse_color) {
+        // NOLINTBEGIN
+        static_assert(std::is_trivial_v<Vertex>);
+        uint32_t VAO, VBO, EBO;
+        CHECKED_GL_CALL(glGenVertexArrays, 1, &VAO);
+        CHECKED_GL_CALL(glGenBuffers, 1, &VBO);
+        CHECKED_GL_CALL(glGenBuffers, 1, &EBO);
 
-Mesh::Mesh(const std::vector<Vertex> &vertices, const std::vector<uint32_t> &indices,
-           std::vector<Texture *> textures) {
-    // NOLINTBEGIN
-    static_assert(std::is_trivial_v<Vertex>);
-    uint32_t VAO, VBO, EBO;
-    CHECKED_GL_CALL(glGenVertexArrays, 1, &VAO);
-    CHECKED_GL_CALL(glGenBuffers, 1, &VBO);
-    CHECKED_GL_CALL(glGenBuffers, 1, &EBO);
+        CHECKED_GL_CALL(glBindVertexArray, VAO);
+        CHECKED_GL_CALL(glBindBuffer, GL_ARRAY_BUFFER, VBO);
+        CHECKED_GL_CALL(glBufferData, GL_ARRAY_BUFFER, vertices.size() * sizeof(vertices[0]), vertices.data(),
+                        GL_STATIC_DRAW);
 
-    CHECKED_GL_CALL(glBindVertexArray, VAO);
-    CHECKED_GL_CALL(glBindBuffer, GL_ARRAY_BUFFER, VBO);
-    CHECKED_GL_CALL(glBufferData, GL_ARRAY_BUFFER, vertices.size() * sizeof(vertices[0]), vertices.data(), GL_STATIC_DRAW);
+        CHECKED_GL_CALL(glBindBuffer, GL_ELEMENT_ARRAY_BUFFER, EBO);
+        CHECKED_GL_CALL(glBufferData, GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(indices[0]), indices.data(),
+                        GL_STATIC_DRAW);
 
-    CHECKED_GL_CALL(glBindBuffer, GL_ELEMENT_ARRAY_BUFFER, EBO);
-    CHECKED_GL_CALL(glBufferData, GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(indices[0]), indices.data(), GL_STATIC_DRAW);
+        CHECKED_GL_CALL(glEnableVertexAttribArray, 0);
+        CHECKED_GL_CALL(glVertexAttribPointer, 0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                        (void *) offsetof(Vertex, Position));
 
-    CHECKED_GL_CALL(glEnableVertexAttribArray, 0);
-    CHECKED_GL_CALL(glVertexAttribPointer, 0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *) offsetof(Vertex, Position));
+        CHECKED_GL_CALL(glEnableVertexAttribArray, 1);
+        CHECKED_GL_CALL(glVertexAttribPointer, 1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                        (void *) offsetof(Vertex, Normal));
 
-    CHECKED_GL_CALL(glEnableVertexAttribArray, 1);
-    CHECKED_GL_CALL(glVertexAttribPointer, 1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *) offsetof(Vertex, Normal));
+        CHECKED_GL_CALL(glEnableVertexAttribArray, 2);
+        CHECKED_GL_CALL(glVertexAttribPointer, 2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                        (void *) offsetof(Vertex, TexCoords));
 
-    CHECKED_GL_CALL(glEnableVertexAttribArray, 2);
-    CHECKED_GL_CALL(glVertexAttribPointer, 2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *) offsetof(Vertex, TexCoords));
+        CHECKED_GL_CALL(glEnableVertexAttribArray, 3);
+        CHECKED_GL_CALL(glVertexAttribPointer, 3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                        (void *) offsetof(Vertex, Tangent));
 
-    CHECKED_GL_CALL(glEnableVertexAttribArray, 3);
-    CHECKED_GL_CALL(glVertexAttribPointer, 3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *) offsetof(Vertex, Tangent));
+        CHECKED_GL_CALL(glEnableVertexAttribArray, 4);
+        CHECKED_GL_CALL(glVertexAttribPointer, 4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                        (void *) offsetof(Vertex, Bitangent));
 
-    CHECKED_GL_CALL(glEnableVertexAttribArray, 4);
-    CHECKED_GL_CALL(glVertexAttribPointer, 4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *) offsetof(Vertex, Bitangent));
-
-    CHECKED_GL_CALL(glBindVertexArray, 0);
-    // NOLINTEND
-    m_vao = VAO;
-    m_num_indices = indices.size();
-    m_textures = std::move(textures);
-}
-
-void Mesh::draw(const Shader *shader) {
-    std::unordered_map<std::string_view, uint32_t> counts;
-    std::string uniform_name;
-    uniform_name.reserve(32);
-    for (int i = 0; i < m_textures.size(); i++) {
-        CHECKED_GL_CALL(glActiveTexture, GL_TEXTURE0 + i);
-        const auto &texture_type = Texture::uniform_name_convention(m_textures[i]->type());
-        uniform_name.append(texture_type);
-        const auto count = (counts[texture_type] += 1);
-        uniform_name.append(std::to_string(count));
-        shader->set_int(uniform_name, i);
-        CHECKED_GL_CALL(glBindTexture, GL_TEXTURE_2D, m_textures[i]->id());
-        uniform_name.clear();
+        CHECKED_GL_CALL(glBindVertexArray, 0);
+        // NOLINTEND
+        m_vao           = VAO;
+        m_num_indices   = indices.size();
+        m_textures      = std::move(textures);
+        m_diffuse_color = diffuse_color;
     }
-    CHECKED_GL_CALL(glBindVertexArray, m_vao);
-    CHECKED_GL_CALL(glDrawElements, GL_TRIANGLES, m_num_indices, GL_UNSIGNED_INT, (void *) 0);
-    CHECKED_GL_CALL(glBindVertexArray, 0);
-}
 
-void Mesh::destroy() {
-    CHECKED_GL_CALL(glDeleteVertexArrays, 1, &m_vao);
-}
+    void Mesh::draw(const Shader *shader) {
+        // Test if shader exists
+        bool has_texture = !m_textures.empty();
+        shader->set_bool("hasTexture", has_texture);
 
-}// namespace engine::resources
+        // 2. Send material colors to m_diffuse_color
+        shader->set_vec3("material_diffuse", m_diffuse_color);
+
+        std::unordered_map<std::string_view, uint32_t> counts;
+        std::string uniform_name;
+        uniform_name.reserve(32);
+        for (int i = 0; i < m_textures.size(); i++) {
+            CHECKED_GL_CALL(glActiveTexture, GL_TEXTURE0 + i);
+            const auto &texture_type = Texture::uniform_name_convention(m_textures[i]->type());
+            uniform_name.append(texture_type);
+            const auto count = (counts[texture_type] += 1);
+            uniform_name.append(std::to_string(count));
+            shader->set_int(uniform_name, i);
+            CHECKED_GL_CALL(glBindTexture, GL_TEXTURE_2D, m_textures[i]->id());
+            uniform_name.clear();
+        }
+        CHECKED_GL_CALL(glBindVertexArray, m_vao);
+        CHECKED_GL_CALL(glDrawElements, GL_TRIANGLES, m_num_indices, GL_UNSIGNED_INT, (void *) 0);
+        CHECKED_GL_CALL(glBindVertexArray, 0);
+    }
+
+    void Mesh::destroy() {
+        CHECKED_GL_CALL(glDeleteVertexArrays, 1, &m_vao);
+    }
+} // namespace engine::resources
